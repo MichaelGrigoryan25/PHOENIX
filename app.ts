49ae2b54106cc6c79c2019789ef7ef033d491d88
Michael <michael.grigoryan25@gmail.com>
2020-06-30 10:55:58 +0400

    Added Ban Module

--- app.ts
+++ app.ts
@@ -126,6 +126,49 @@ client.on('message', async msg => {
       }
     }
   }
+
+  //Ban Module
+  if (msg.member.hasPermission("BAN_MEMBERS")) {
+    if (msg.content.startsWith(`${prefix}ban`)) {
+            // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+      const user = msg.mentions.users.first();
+      var ping = new Date().getTime() - msg.createdTimestamp;
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = msg.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Kick the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           */
+          member
+            .ban('')
+            .then(() => {
+              // We let the message author know we were able to kick the person
+              msg.reply(`Successfully banned ${user.tag}` + " | " + "Ping time: " + ping);
+            })
+            .catch(err => {
+              // An error happened
+              // This is generally due to the bot not being able to kick the member,
+              // either due to missing permissions or role hierarchy
+              msg.reply('I was unable to ban the member');
+              // Log the error
+              console.error(err);
+            });
+        } else {
+          // The mentioned user isn't in this guild
+          msg.reply("That user isn't in this guild!");
+        }
+        // Otherwise, if no user was mentioned
+      } else {
+        msg.reply("You didn't mention the user to ban!");
+      }
+    }
+  }
 });
 
 client.login(token);
