4249b70ffb39322fa0b6f8e0ff1f09253ccae765
Michael <michael.grigoryan25@gmail.com>
2020-06-30 10:41:23 +0400

    Fixed Kick Module

--- app.ts
+++ app.ts
@@ -31,7 +31,7 @@ client.on('guildMemberAdd', member => {
 //Once receive a message
 client.on('message', async msg => {
   // Ignore messages that aren't from a guild
-  if(!msg.guild) return;
+  if (!msg.guild) return;
   const args = msg.content.split(" ")
 
   //Help Module
@@ -44,8 +44,8 @@ client.on('message', async msg => {
       .addFields(
         { name: '$help', value: 'List all the available commands.' },
         { name: '$ping or $pong', value: 'PONG! or PING!' },
-        { name: '$kick', value: 'Kick a user from the guild.' },
-        { name: '$clear', value: 'Clear the specified amount of messages in the chat.' },
+        { name: '$kick + mention', value: 'Kick a user from the guild.' },
+        { name: '$clear + number', value: 'Clear the specified amount of messages in the chat.' },
       )
       .setTimestamp()
       .setFooter('PHOENIX');
@@ -85,42 +85,45 @@ client.on('message', async msg => {
   }
 
   //Kick Module
-  if (msg.content.startsWith(`${prefix}kick`)) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = msg.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = msg.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            msg.reply(`Successfully kicked ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            msg.reply('I was unable to kick the member');
-            // Log the error
-            console.error(err);
-          });
+  if (msg.member.hasPermission("KICK_MEMBERS")) {
+    if (msg.content.startsWith(`${prefix}kick`)) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+      const user = msg.mentions.users.first();
+      var ping = new Date().getTime() - msg.createdTimestamp;
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = msg.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Kick the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           */
+          member
+            .kick('Optional reason that will display in the audit logs')
+            .then(() => {
+              // We let the message author know we were able to kick the person
+              msg.reply(`Successfully kicked ${user.tag}` + " | " + "Ping time: " + ping);
+            })
+            .catch(err => {
+              // An error happened
+              // This is generally due to the bot not being able to kick the member,
+              // either due to missing permissions or role hierarchy
+              msg.reply('I was unable to kick the member');
+              // Log the error
+              console.error(err);
+            });
+        } else {
+          // The mentioned user isn't in this guild
+          msg.reply("That user isn't in this guild!");
+        }
+        // Otherwise, if no user was mentioned
       } else {
-        // The mentioned user isn't in this guild
-        msg.reply("That user isn't in this guild!");
+        msg.reply("You didn't mention the user to kick!");
       }
-      // Otherwise, if no user was mentioned
-    } else {
-      msg.reply("You didn't mention the user to kick!");
     }
   }
 });
