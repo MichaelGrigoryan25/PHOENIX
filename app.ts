e0f674f05aaf508ab1831891b40e39278746feba
Michael <michael.grigoryan25@gmail.com>
2020-06-30 10:06:07 +0400

    Add Kick Module

--- app.ts
+++ app.ts
@@ -30,6 +30,8 @@ client.on('guildMemberAdd', member => {
 
 //Once receive a message
 client.on('message', async msg => {
+  // Ignore messages that aren't from a guild
+  if(!msg.guild) return;
   const args = msg.content.split(" ")
 
   //Help
@@ -90,6 +92,46 @@ client.on('message', async msg => {
         .catch(error => {
           msg.reply(`Couldn't delete messages because of: ${error}`);
         });
+    }
+  }
+
+  //Kick
+  if (msg.content.startsWith(`${prefix}kick`)) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = msg.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = msg.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            msg.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            msg.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        msg.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      msg.reply("You didn't mention the user to kick!");
     }
   }
 });
